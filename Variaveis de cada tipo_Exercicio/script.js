bc8cff7bec4bfcec3ea1deefe57b6c1480542bfc
Daniela Cruz <138163183+danielacruz0@users.noreply.github.com>
2023-07-22 15:43:51 +0100

    Add files via upload

---
+++ Variaveis de cada tipo_Exercicio/script.js
@@ -0,0 +1,9 @@
+var nome = 'Daniela';
+
+let morada = 'Braga';
+
+const dataNascimento = 1994;
+
+var estudanteTrusted = true;
+
+console.log('Chamo-me' + nome + 'nasci em' + dataNascimento + 'estou a morar em' + morada + 'Estas a estudar na Trusted School?' + estudanteTrusted)
